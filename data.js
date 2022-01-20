ec10d5275ea64135382ddb4aff7e81d8527a8905
Jacegk1999 <Jacegk1999@gmail.com>
2022-01-19 16:45:51 -0800

    updating data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+export const people = [
+    {
+        id : 1,
+        name: 'Jace',
+        age: 22,
+        bio: 'Brown hair, Brown eyes,'
+        hobbies:'Swing dancing','video games','cooking'
+    },
+    {
+        id : ,
+        name: '',
+        age: ,
+        bio: ','
+        hobbies:'','',''
+    },
+    {
+        id : ,
+        name: '',
+        age: ,
+        bio: ','
+        hobbies:'','',''
+    },
+]
