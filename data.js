fdce7b02d9616cf2a3dbdbab3cc7c3dcf1a0c60f
Jacegk1999 <Jacegk1999@gmail.com>
2022-01-21 12:47:04 -0800

    Added headers to all sections

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 export const people = [
     {
         id : 1,
-        name: 'Jace',
+        name: 'Jace (He/Him)',
         age: 22,
         bio: 'Brown hair, Brown eyes',
         hobbies:['Swing dancing', 'video games', 'cooking'],
